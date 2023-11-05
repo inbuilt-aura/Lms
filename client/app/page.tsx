f564de45589d6a22dab1bca0abc303a5771f45fb
Aman <developeramanyt@gmail.com>
2023-11-05 21:36:47 +0530

    uPDATED CLIENT FOLDER

---
+++ client/app/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { FC, useState } from "react";
+import Heading from "./utils/Heading";
+import Header from "./components/Header";
+interface Props {}
+
+const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  return (
+    <div>
+      <Heading
+        title="ECoders"
+        description="ECoders is an online e-learning platform for students or professional developers to come and join us to build something amazing."
+        keywords="Programming, development, MERN stack, software, learn, tutorials,coding"
+      />
+      <Header
+      open={open}
+      setOpen={setOpen}
+      activeItem={activeItem}
+      />
+    </div>
+  );
+};
+
+export default Page;
